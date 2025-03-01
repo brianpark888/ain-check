9d0225d7bf8c3319d09c6a20d363e20b15ddad24
brianpark888 <126150953+brianpark888@users.noreply.github.com>
2025-02-28 17:05:38 -0800

    debug

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,17 @@
 import InteractiveClient from "@/components/InteractiveClient";
 
 export default async function Home() {
-  // Fetch GeoJSON on the server (Runs only on the server)
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/fetch-geojson`);
+  // // Fetch GeoJSON on the server (Runs only on the server)
+  // const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/fetch-geojson`);
 
-  const geoJSON = await response.json();
+  // const geoJSON = await response.json();
 
   return (
     <main className="container mx-auto p-4 max-w-4xl">
       <h1 className="text-2xl font-bold mb-6">AIN Neighborhood Change Checker</h1>
 
       {/* Pass data to Client Component */}
-      <InteractiveClient geoJSON={geoJSON} />
+      <InteractiveClient />
     </main>
   );
 }
